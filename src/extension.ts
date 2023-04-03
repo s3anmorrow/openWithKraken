efac05865000e58f5420cf72afb1f00fad06d3c0
Sean Morrow <s3anmorrow@gmail.com>
2023-04-03 19:47:27 -0300

    added support for multi-root workspace

--- src/extension.ts
+++ src/extension.ts
@@ -1,37 +1,39 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { exec } from 'child_process';
 
 // command identifier
 const COMMAND_ID:string = 'openwithkraken.open';
+
 let myStatusBarItem: vscode.StatusBarItem;
 
 // --------------------------------------- private methods
 
 // --------------------------------------- boilerplate methods
 export function activate({ subscriptions }: vscode.ExtensionContext) {
-	// register a command that is invoked when the status bar
-	// item is selected
+	// register a command that is invoked when the status bar item is selected
 	subscriptions.push(vscode.commands.registerCommand(COMMAND_ID, () => {
 
 		console.log(vscode.workspace.workspaceFolders);
 
 
-		vscode.workspace.workspaceFolders?.forEach(folder => {
-			console.log(folder.uri.path);
 
-			const cp = require('child_process');
-			cp.exec(`gitkraken -p ${folder.uri.path}`, (err:any, stdout:any, stderr:any) => {
+
+		// open gitkraken for all folders in workspace
+		vscode.workspace.workspaceFolders?.forEach(folder => {
+
+			// TODO error check for undefined meaning no workspace folder is open
+			// console.log(`${folder.uri.path}`);
+			
+			// invoke command to open gitkraken for each folder in the workspace (works for single folder or multi-root workspace)
+			exec(`gitkraken -p ${folder.uri.path}`, (err:any, stdout:any, stderr:any) => {
 				if (err) {
 					console.log(`ERROR: {err.message}`);
 					vscode.window.showInformationMessage(`ERROR: Do you have GitKraken installed? [${err.message}]`);
 				}
 			});
 		});
-
-
-		
-
 	}));
 
 	// create a new status bar item that we can now manage
@@ -46,10 +48,6 @@ export function activate({ subscriptions }: vscode.ExtensionContext) {
 
 	console.log("OpenWithKraken activated!");
 
-	// // register some listener that make sure the status bar 
-	// // item always up-to-date
-	// subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
-	// subscriptions.push(vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem));
 
 	// update status bar item once at start
 	myStatusBarItem.text = `$(repo-forked) GitKraken`;
